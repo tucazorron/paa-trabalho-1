1c9ffd55ffd08dd6c6e44941f830a3b4ef5a1a29
artur zorron <tucazorron@gmail.com>
2021-08-31 13:50:21 -0300

    change to merge sort solution

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -1,52 +1,82 @@
 #include <iostream>
+
 using namespace std;
 
-int insertion_sort(int data[], int n)
+int merge(int array[], int aux[], int left, int middle, int right)
 {
-    int i, j, tmp, swaps = 0;
 
-    for (i = 1; i < n; i++)
+    int swaps = 0;
+
+    int l = left;
+    int m = middle;
+    int r = left;
+
+    while ((l <= middle - 1) && (m <= right))
     {
-        j = i;
-        tmp = data[i];
-        while (j > 0 && tmp < data[j - 1])
+        if (array[l] <= array[m])
         {
-            data[j] = data[j - 1];
-            j--;
+            aux[r++] = array[l++];
         }
-        data[j] = tmp;
-        swaps++;
+
+        else
+        {
+            aux[r++] = array[m++];
+            swaps += (middle - l);
+        }
+    }
+
+    while (l <= middle - 1)
+    {
+        aux[r++] = array[l++];
+    }
+
+    while (m <= right)
+    {
+        aux[r++] = array[m++];
+    }
+
+    for (l = left; l <= right; l++)
+    {
+        array[l] = aux[l];
+    }
+
+    return swaps;
+}
+
+int merge_sort(int array[], int aux[], int left, int right)
+{
+    int swaps = 0;
+
+    if (right > left)
+    {
+        int middle = (left + right) / 2;
+
+        swaps = merge_sort(array, aux, left, middle);
+        swaps += merge_sort(array, aux, middle + 1, right);
+
+        swaps += merge(array, aux, left, middle + 1, right);
     }
     return swaps;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
     {
-        int t;
-        cin >> t;
-        char input[1000];
-        cin >> input;
-        int array[t], k = 0;
-        for (int j = 0; j < t; j++)
+        int n;
+        scanf("%d", &n);
+
+        int array[n];
+        for (int i = 0; i < n; i++)
         {
-            array[j] = 0;
+            scanf("%d", &array[i]);
         }
-        for (int j = 0; input[j] != '\0'; j++)
-        {
-            if (input[j] == ' ')
-            {
-                k++;
-            }
-            else
-            {
-                array[k] = array[k] * 10 + (input[j] - 48);
-            }
-        }
-        int swaps = insertion_sort(array, t);
-        cout << swaps << endl;
+        int aux[n];
+        int swaps = merge_sort(array, aux, 0, n - 1);
+        printf("%d\n", swaps);
     }
+    return 0;
 }
